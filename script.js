af17364ab1994455b15eb8e24efcf6c6dd421515
Conner Bachmann <bac@christmann.info>
2023-11-15 19:53:50 +0100

    feat: add ics button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const countDownDate = new Date("Nov 30, 2023 00:00:00").getTime();
+const countDownDate = new Date("Nov 30, 2023 12:00:00");
 
+document.addEventListener("DOMContentLoaded", function () {
   function updateCountdown() {
     const now = new Date().getTime();
 
-    const distance = countDownDate - now;
+    const distance = countDownDate.getTime() - now;
 
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
@@ -32,3 +32,55 @@ document.addEventListener("DOMContentLoaded", function () {
     return time < 10 ? "0" + time : time;
   }
 });
+
+function addToCalendar() {
+  const event = {
+    title: "CodeSphere Countdown Challenge 🎃",
+    date: countDownDate,
+    location: "Online",
+  };
+
+  console.log(event);
+
+  // Generate the iCalendar (.ics) content
+  const icsContent = generateICS(event);
+
+  // Create a Blob with the content
+  const blob = new Blob([icsContent], { type: "text/calendar" });
+
+  // Create a download link and trigger the click event
+  const link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  link.download = "event.ics";
+  link.click();
+}
+
+function formatDateForICS(date) {
+  function pad(number) {
+    if (number < 10) {
+      return "0" + number;
+    }
+    return number;
+  }
+
+  const year = date.getUTCFullYear();
+  const month = pad(date.getUTCMonth() + 1); // Months are zero-indexed
+  const day = pad(date.getUTCDate());
+  const hours = pad(date.getUTCHours());
+  const minutes = pad(date.getUTCMinutes());
+  const seconds = pad(date.getUTCSeconds());
+
+  return `${year}${month}${day}T${hours}${minutes}${seconds}00Z`;
+}
+
+function generateICS(event) {
+  return `BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:${event.title}
+DTSTART:${formatDateForICS(event.date)}
+LOCATION:${event.location}
+DESCRIPTION:${event.title}
+END:VEVENT
+END:VCALENDAR`;
+}

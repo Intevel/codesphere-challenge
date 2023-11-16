0f00a7a75fe5e6b05daee73b9beae43298bb0480
Conner Bachmann <github@conner-bachmann.de>
2023-11-16 11:19:03 +0100

    refactor: use express server

--- public/script.js
+++ public/script.js
@@ -1,34 +1,24 @@
 const countDownDate = new Date("Nov 30, 2023 12:00:00");
+let ntpServerTime;
 
-document.addEventListener("DOMContentLoaded", function () {
-  const radialGradient = document.getElementById("radialGradient");
-  const textContainer = document.getElementById("textContainer");
-  const radius = 400;
+async function fetchNTPServerTime() {
+  const ntpServerUrl = "/getDateTime";
 
-  function handleMouseMove(event) {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
+  const response = await fetch(ntpServerUrl);
+  const ntpResponse = await response.json();
 
-    radialGradient.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(90, 36, 196, 0.4) 0%, rgba(90, 36, 196, 0) 20%)`;
-
-    const textRect = textContainer.getBoundingClientRect();
-    const textCenterX = textRect.left + textRect.width / 2;
-    const textCenterY = textRect.top + textRect.height / 2;
-    const distance = Math.sqrt(
-      (mouseX - textCenterX) ** 2 + (mouseY - textCenterY) ** 2
-    );
-
-    const minOpacity = 0.1;
-    const opacity =
-      1 - (Math.min(distance, radius) / radius) * (1 - minOpacity);
-
-    textContainer.style.opacity = opacity;
+  if (ntpResponse.dateTime) {
+    ntpServerTime = new Date(ntpResponse.dateTime);
   }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  await fetchNTPServerTime();
 
   window.addEventListener("mousemove", handleMouseMove);
 
   function updateCountdown() {
-    const now = new Date().getTime();
+    const now = ntpServerTime ? ntpServerTime.getTime() : Date.now();
 
     const distance = countDownDate.getTime() - now;
 
@@ -53,11 +43,34 @@ document.addEventListener("DOMContentLoaded", function () {
   updateCountdown();
 
   const x = setInterval(updateCountdown, 1000);
-
-  function formatTime(time) {
-    return time < 10 ? "0" + time : time;
-  }
 });
+
+function handleMouseMove(event) {
+  const radialGradient = document.getElementById("radialGradient");
+  const textContainer = document.getElementById("textContainer");
+  const radius = 400;
+
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  radialGradient.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(90, 36, 196, 0.4) 0%, rgba(90, 36, 196, 0) 20%)`;
+
+  const textRect = textContainer.getBoundingClientRect();
+  const textCenterX = textRect.left + textRect.width / 2;
+  const textCenterY = textRect.top + textRect.height / 2;
+  const distance = Math.sqrt(
+    (mouseX - textCenterX) ** 2 + (mouseY - textCenterY) ** 2
+  );
+
+  const minOpacity = 0.1;
+  const opacity = 1 - (Math.min(distance, radius) / radius) * (1 - minOpacity);
+
+  textContainer.style.opacity = opacity;
+}
+
+function formatTime(time) {
+  return time < 10 ? "0" + time : time;
+}
 
 function addToCalendar() {
   const event = {
